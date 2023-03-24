ad77158b1d4eb884cbf96f00bd7aee5426c7caff
KatyBeard <borodashkina.ekaterina@yandex.ru>
2023-03-24 22:03:44 +0500

    Added folder Lecture1

---
+++ Lecture1/program.java
@@ -0,0 +1,20 @@
+/**
+ * program
+ */
+public class program {
+    public static void main(String[] args) {
+        // String s = null;
+
+        // System.out.println(s);
+
+        // Типы данных
+        // float f = 12.3f;
+        // double d = 54.51;
+        // double d1 = 564.156d;
+        int a = 123;
+        a = a-- - --a;
+        System.out.println(a);
+
+    }
+    
+}
